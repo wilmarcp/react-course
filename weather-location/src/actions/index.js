a7dc54ad476d29640f60297eddac610d6c7c157e
wilmar_castano <wilmar.castano@tecnofactor.com>
2020-02-03 07:38:46 -0500

    Add redux

--- weather-location/src/actions/index.js
+++ weather-location/src/actions/index.js
@@ -1,2 +1,20 @@
+import transformForecast from './../services/transformForecast';
+import getUrlForecastByCity from './../services/getUrlForecastByCity';
+
 export const SET_CITY = "SET_CITY";
 export const setCity = payload => ({type: SET_CITY, payload});
+
+export const fetchForecast = payload => {
+    return dispatch => {
+        fetch(getUrlForecastByCity(this.props.city)).then(
+            data => data.json()
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+                const forecastData = transformForecast(weather_data);
+                this.setState({forecastData});
+                console.log(forecastData);
+            }
+        );
+    }
+}
--- weather-location/src/store/index.js
+++ weather-location/src/store/index.js
@@ -1,13 +1,22 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import {city} from './../reducers/city'
 
 
-const initialState = {}
+const initialState = {city: "Buenos Aires"}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+    city, 
+    initialState,
+    composeEnhancers(applyMiddleware(thunk)));
 
 
+/*const initialState = {city: "Buenos Aires"}
 export const store = createStore(
     city, 
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
+*/
